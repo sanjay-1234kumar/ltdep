55199edc83ecf488925b6158231f91564bce1d76
Business Account <chandelsanjay300@gmail.com>
2022-10-07 08:39:47 +0530

    first commit

---
+++ build/precache-manifest.613150667e5424a1d1c211396a609440.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c23f8ad7206cb9ddfa1ab47d0642ab29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56b390bb1ff394caeda8",
+    "url": "/static/js/2.55d3eea9.chunk.js"
+  },
+  {
+    "revision": "b19bf6435cbcfbee164171e077e1076b",
+    "url": "/static/js/2.55d3eea9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c995892af5c18262842",
+    "url": "/static/js/main.d253eecc.chunk.js"
+  },
+  {
+    "revision": "5820d58c60ab3b1375ad",
+    "url": "/static/js/runtime-main.99b514f4.js"
+  }
+]);
